5bc63d0377feb6f39fa2a0ac28d7d07af97a2db8
DerekChou <derekchou3704@gmail.com>
2021-06-13 20:56:14 +0800

    Setting states for the buttons

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,33 +1,45 @@
 const socket = io()
 
+// Elements 
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocation = document.querySelector('#send-location')
+
 socket.on('message', (message) => {
     console.log(message)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
+    // disable
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
     // the name after elements is the name we give for the name attri. of input
-    
     const message = e.target.elements.message.value
+
     socket.emit('sendMessage', message, (error) => {
+    // enable
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
         error 
             ? console.log(error)
             : console.log('The message was delivered', message)
     })
 })
 
-document.querySelector('#send-location').addEventListener('click', (e) => {
+$sendLocation.addEventListener('click', (e) => {
     !navigator.geolocation 
         ? alert('Geolocation is not supported on current browser')
         : navigator.geolocation.getCurrentPosition(position => {
+            $sendLocation.setAttribute('disabled', 'disabled')
             socket.emit('sendLocation', { 
                 lattitude:position.coords.latitude, 
                 longitude: position.coords.longitude 
-            }, (error) => {
+            }, () => {
                 // client acknowledgement fn
-                error 
-                    ? console.log(error)
-                    : console.log('Location was delivered')
+                $sendLocation.removeAttribute('disabled')
             })
         })
 
